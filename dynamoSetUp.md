c075841883d4c83bae7eb09a116317537391eb1b
Arditti93 <alex.arditti@hotmail.com>
2022-10-25 15:35:17 +0100

    added lambda code

---
+++ dynamoSetUp.md
@@ -0,0 +1,5 @@
+Name = made_up_users
+Partition key = id 
+Sort Key = username
+
+read capacity = On-Demand
---
+++ lambda_handler.py
@@ -0,0 +1,27 @@
+import json
+import boto3
+import uuid
+
+# To use Boto3, you must first import it and indicate which service or services you're going to use:
+dynamodb = boto3.resource('dynamodb', region_name='eu-west-2')
+table = dynamodb.Table('made_up_users')
+
+def lambda_handler(event, context):
+    data = json.loads(event['body'])
+    
+    try:
+        response = table.put_item(
+            Item={
+                'id': str(uuid.uuid4()),
+                'username': data['username'],
+                'email': data['email'],
+                'password': data['password']
+            }
+        )
+    except Exception as e:
+        print (e)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
